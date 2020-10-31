6ef2215898a2562a098d9cecfbf6608c2e3e0d9f
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-10-31 11:02:40 +0100

    Aspects

--- src/main/scala/io/github/vigoo/awsquery/Main.scala
+++ src/main/scala/io/github/vigoo/awsquery/Main.scala
@@ -6,26 +6,38 @@ import io.github.vigoo.awsquery.report._
 import io.github.vigoo.awsquery.report.cache._
 import io.github.vigoo.awsquery.report.render.{Rendering, renderAsg, renderEc2Instance, renderElb}
 import io.github.vigoo.clipp
+import io.github.vigoo.clipp.ParserFailure
 import io.github.vigoo.clipp.syntax._
 import io.github.vigoo.clipp.parsers._
 import io.github.vigoo.clipp.zioapi.config.{ClippConfig, parameters}
 import io.github.vigoo.zioaws._
-import io.github.vigoo.zioaws.core.AwsError
-import io.github.vigoo.zioaws.core.config.CommonAwsConfig
+import io.github.vigoo.zioaws.autoscaling.AutoScaling
+import io.github.vigoo.zioaws.core.{AwsError, GenericAwsError, aspects}
+import io.github.vigoo.zioaws.core.aspects.{AwsCallAspect, Described}
+import io.github.vigoo.zioaws.core.config.{AwsConfig, CommonAwsConfig}
+import io.github.vigoo.zioaws.ec2.Ec2
+import io.github.vigoo.zioaws.elasticbeanstalk.ElasticBeanstalk
+import io.github.vigoo.zioaws.elasticloadbalancing.ElasticLoadBalancing
+import nl.vroste.rezilience.CircuitBreaker.State
+import nl.vroste.rezilience.{CircuitBreaker, Policy, Retry, TrippingStrategy}
 import org.apache.logging.log4j.core.config.Configurator
 import org.apache.logging.log4j.{Level, LogManager}
 import org.apache.logging.log4j.core.config.builder.api.{ConfigurationBuilder, ConfigurationBuilderFactory}
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.awscore.exception.AwsServiceException
+import software.amazon.awssdk.core.exception.SdkException
 import software.amazon.awssdk.regions.Region
 import zio._
 import zio.clock.Clock
 import zio.console.Console
+import zio.duration.durationInt
 import zio.logging._
 import zio.logging.slf4j._
 import zio.query.ZQuery
 
 object Main extends App {
+
   case class Parameters(verbose: Boolean,
                         searchInput: String)
 
@@ -54,9 +66,74 @@ object Main extends App {
   }
 
   // TODOs
-  // rate limiting as zio-aws aspects
   // "execution graph dump" aspect for generating diagrams for the post?
   // typesafe pprint monad
+
+  private def throttlingPolicy: ZManaged[Clock with Logging, Nothing, Policy[AwsError]] =
+    for {
+      logging <- ZManaged.environment[Logging]
+      cb <- CircuitBreaker.make[AwsError](
+        trippingStrategy = TrippingStrategy.failureCount(1),
+        resetPolicy = Retry.Schedules.exponentialBackoff(1.second, 1.minute),
+        isFailure = {
+          case GenericAwsError(error: AwsServiceException) if error.isThrottlingException => true
+        },
+        onStateChange = {
+          case State.Closed =>
+            log.info(s"Circuit breaker closed").provide(logging)
+          case State.HalfOpen =>
+            log.info(s"Circuit breaker half open").provide(logging)
+          case State.Open =>
+            log.info(s"Circuit breaker open").provide(logging)
+        }
+      )
+    } yield cb.toPolicy
+
+  private def awsQuery(): ZIO[Clock with Console with Logging with ClippConfig[Parameters], Nothing, ExitCode] =
+    throttlingPolicy.use { policy =>
+      val throttling = new AwsCallAspect[Any] {
+        override def apply[R1, A](f: ZIO[R1, AwsError, aspects.Described[A]]): ZIO[R1, AwsError, aspects.Described[A]] =
+          policy(f).mapError {
+            case Policy.WrappedError(e) => e
+            case Policy.BulkheadRejection => AwsError.fromThrowable(new RuntimeException(s"Bulkhead rejection"))
+            case Policy.CircuitBreakerOpen => AwsError.fromThrowable(new RuntimeException(s"AWS rate limit exceeded"))
+          }
+      }
+
+      val callLogging: AwsCallAspect[Logging] =
+        new AwsCallAspect[Logging] {
+          override final def apply[R1 <: Logging, A](f: ZIO[R1, AwsError, Described[A]]): ZIO[R1, AwsError, Described[A]] =
+            f.flatMap { case r @ Described(_, description) =>
+              log.info(s"[${description.service}/${description.operation}]").as(r)
+            }
+        }
+
+      val commonConfig = ZLayer.succeed(CommonAwsConfig(
+        region = Some(Region.US_EAST_1),
+        credentialsProvider = DefaultCredentialsProvider.create(),
+        endpointOverride = None,
+        commonClientConfig = None
+      ))
+
+      val awsCore = (netty.default ++ commonConfig) >>> core.config.configured()
+
+      val awsClients =
+          ec2.live @@ (throttling >>> callLogging) ++
+          elasticloadbalancing.live @@ (throttling >>> callLogging) ++
+          elasticbeanstalk.live @@ (throttling >>> callLogging) ++
+          autoscaling.live @@ (throttling >>> callLogging)
+
+      val finalLayer =
+        ((ZLayer.service[Logger[String]] ++ awsCore) >>> awsClients) ++
+          ((Console.any ++ cache.live) >+> render.live)
+
+      runQuery()
+        .provideSomeLayer[Clock with Console with Logging with ClippConfig[Parameters]](finalLayer)
+        .tapError { error =>
+          console.putStrLnErr(error.toString)
+        }
+        .exitCode
+    }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
     val paramSpec = for {
@@ -91,33 +168,11 @@ object Main extends App {
 
     val logging = log4j2 >>> Slf4jLogger.make { (_, message) => message }
 
-    val commonConfig = ZLayer.succeed(CommonAwsConfig(
-      region = Some(Region.US_EAST_1),
-      credentialsProvider = DefaultCredentialsProvider.create(),
-      endpointOverride = None,
-      commonClientConfig = None
-    ))
-
-    val awsCore = (netty.default ++ commonConfig) >>> core.config.configured()
-
-    val awsClients =
-        ec2.live ++
-        elasticloadbalancing.live ++
-        elasticbeanstalk.live ++
-        autoscaling.live
-
-    val finalLayer =
-      (awsCore >>> awsClients) ++
-        (params >+> logging) ++
-        ((Console.any ++ cache.live) >+> render.live)
-
     for {
-      _ <- runQuery()
-        .provideLayer(finalLayer)
-        .catchAll { error =>
-          console.putStrLnErr(error.toString)
-        }
+      result <- awsQuery()
+        .provideCustomLayer(params >+> logging)
+        .catchAll { failure: ParserFailure => ZIO.succeed(ExitCode.failure) }
       _ <- ZIO.effect(LogManager.shutdown()).orDie
-    } yield ExitCode.success
+    } yield result
   }
 }
