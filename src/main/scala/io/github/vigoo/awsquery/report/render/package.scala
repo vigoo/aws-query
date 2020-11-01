c33cedc405ab9039e7e93ca5fb6dab6e02c86500
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-11-01 14:28:01 +0100

    Fixes

--- src/main/scala/io/github/vigoo/awsquery/report/render/package.scala
+++ src/main/scala/io/github/vigoo/awsquery/report/render/package.scala
@@ -81,18 +81,26 @@ package object render {
   private trait PrettyConsole {
     protected val console: Console.Service
 
-    private def runImpl[A](p: Print[A], indentation: String, afterNL: Boolean): UIO[A] =
+    private case class PrettyState(indentation: String, afterNL: Boolean)
+
+    private def printFlatMap[A, B](a: Print[A], f: A => Print[B], state: PrettyState): UIO[(B, PrettyState)] =
+      for {
+        r1 <- runImpl(a, state)
+        r2 <- runImpl(f(r1._1), r1._2)
+      } yield r2
+
+    private def runImpl[A](p: Print[A], state: PrettyState): UIO[(A, PrettyState)] =
       p match {
-        case Pure(a) => ZIO.succeed(a)
-        case PrintS(s) => ZIO.when(afterNL)(console.putStr(indentation)) *> console.putStr(s)
-        case PrintModified(s, modifiers) => ZIO.when(afterNL)(console.putStr(indentation)) *> console.putStr(s"${modifiers}$s$RESET")
-        case PrintNL => console.putStrLn("")
-        case PrintIndented(f) => runImpl(f, indentation = indentation + "  ", afterNL = false)
-        case PrintFlatMap(a, f) => runImpl(a, indentation, afterNL) >>= f.andThen(p => runImpl(p, indentation, a == PrintNL))
-        case PrintEffect(f) => f
+        case Pure(a) => ZIO.succeed((a, state))
+        case PrintS(s) => ZIO.when(state.afterNL)(console.putStr(state.indentation)) *> console.putStr(s).as(((), state.copy(afterNL = false)))
+        case PrintModified(s, modifiers) => ZIO.when(state.afterNL)(console.putStr(state.indentation)) *> console.putStr(s"${modifiers}$s$RESET").as(((), state.copy(afterNL = false)))
+        case PrintNL => if (state.afterNL) ZIO.succeed(((), state)) else console.putStrLn("").as(((), state.copy(afterNL = true)))
+        case PrintIndented(f) => runImpl(f, state.copy(indentation = state.indentation + "  ")).map { case (a, s) => (a, s.copy(indentation = state.indentation)) }
+        case PrintFlatMap(a, f) => printFlatMap(a, f, state)
+        case PrintEffect(f) => f.map((_, state))
       }
 
-    def run[A](p: Print[A]): UIO[A] = runImpl(p, "", afterNL = false)
+    def run[A](p: Print[A]): UIO[A] = runImpl(p, PrettyState("", afterNL = false)).map(_._1)
   }
 
   private class PrettyConsoleRendering(state: Ref[State],
@@ -154,11 +162,7 @@ package object render {
             keyword("Listener") <:> listenerList(elb.listeners) \\
             keyword(s"${elb.availabilityZones.size} availability zones") <:> details(elb.availabilityZones.mkString(" ")) \\
             keyword(s"${elb.instancesIds.size} instances") <:> details(elb.instancesIds.mkString(" ")) \\
-            ifDefined(elb.ebEnv) { e =>
-              indented {
-                ebEnv(e)
-              } *> newLine
-            }
+            ifDefined(elb.ebEnv)(ebEnv)
           }
       }
 
@@ -177,11 +181,7 @@ package object render {
             keyword("Instance profile") <:> normal(ec2.instanceProfileArn) <-> details(s"(${ec2.instanceProfileId})") \\
             keyword("SSH key name") <:> normal(ec2.sshKeyName) \\
             keyword("Launched at") <:> normal(ec2.launchedAt.toString) \\
-            ifDefined(ec2.elb) { e =>
-              indented {
-                elb(e, Some(s"the instance ${ec2.instanceId} is registered into this ELB "))
-              } *> newLine
-            }
+            ifDefined(ec2.elb)(elb(_, Some(s"the instance ${ec2.instanceId} is registered into this ELB ")))
           }
       }
 
@@ -202,31 +202,37 @@ package object render {
     private def ebApp(report: LinkedReport[EbAppKey, EbAppReport]): Print[Unit] =
       ifNotVisitedYet(report) { app =>
         sectionHeader("Beanstalk/App") <-> normal("Application") <-> highlighted(app.name) \\
-        keyword("AWS Console") <:> link(s"https://console.aws.amazon.com/elasticbeanstalk/home?region=${app.region}#/application/overview?applicationName=${app.name}") \\
-        normal(s"${app.numberOfVersions} versions") \\
-        app.ebEnvs.foreach_(ebEnv)
+        indented {
+          keyword("AWS Console") <:> link(s"https://console.aws.amazon.com/elasticbeanstalk/home?region=${app.region}#/application/overview?applicationName=${app.name}") \\
+          normal(s"${app.numberOfVersions} versions") \\
+          app.ebEnvs.foreach_(ebEnv)
+        }
       }
 
     private def asg(report: LinkedReport[AsgKey, AsgReport]): Print[Unit] =
       ifNotVisitedYet(report) { asg =>
         sectionHeader("EC2/ASG") <-> normal("Auto Scaling group") <-> highlighted(asg.id) <-> normal(s"in ${asg.region}") \\
-        keyword("AWS Console") <:> link(s"https://console.aws.amazon.com/ec2/autoscaling/home?region=${asg.region}#AutoScalingGroups:id=${asg.id};filter=${asg.id};view=details") \\
-        keyword("Instances") <:> normal(s"current=${asg.instanceCount} min=${asg.minSize} max=${asg.maxSize} desired=${asg.desiredCapacity}")
-        keyword("Tags") <:> newLine *> indented(keyValueList(asg.tags)) *>
-        keyword("Created at") <:> normal(asg.createdAt.toString) \\
-        asg.loadBalancers.foreach_(elb(_, None)) \\
-        ifDefined(asg.ebEnv)(ebEnv) \\
-        launchConfig(asg.launchConfiguration)
+          indented {
+            keyword("AWS Console") <:> link(s"https://console.aws.amazon.com/ec2/autoscaling/home?region=${asg.region}#AutoScalingGroups:id=${asg.id};filter=${asg.id};view=details") \\
+            keyword("Instances") <:> normal(s"current=${asg.instanceCount} min=${asg.minSize} max=${asg.maxSize} desired=${asg.desiredCapacity}")
+            keyword("Tags") <:> newLine *> indented(keyValueList(asg.tags)) *>
+            keyword("Created at") <:> normal(asg.createdAt.toString) \\
+            asg.loadBalancers.foreach_(elb(_, None)) \\
+            ifDefined(asg.ebEnv)(ebEnv) \\
+            launchConfig(asg.launchConfiguration)
+          }
       }
 
     private def launchConfig(report: LinkedReport[LaunchConfigKey, LaunchConfigReport]): Print[Unit] =
       ifNotVisitedYet(report) { lc =>
         sectionHeader("EC2/LaunchConfiguration") <-> highlighted(lc.name) <-> normal(s"in ${lc.region}") \\
-        keyword("Created at") <:> normal(lc.createdAt.toString) \\
-        keyword("AMI") <:> normal(lc.amiId) \\
-        ifDefined(lc.instanceProfileArn)(arn => keyword("IAM Instance Profile") <:> details(arn)) \\
-        keyword("Instance type") <:> normal(lc.instanceType) \\
-        keyword("Security groups") <:> details(lc.securityGroups.mkString(" "))
+        indented {
+          keyword("Created at") <:> normal(lc.createdAt.toString) \\
+          keyword("AMI") <:> normal(lc.amiId) \\
+          ifDefined(lc.instanceProfileArn)(arn => keyword("IAM Instance Profile") <:> details(arn)) \\
+          keyword("Instance type") <:> normal(lc.instanceType) \\
+          keyword("Security groups") <:> details(lc.securityGroups.mkString(" "))
+        }
       }
 
     override def renderElb(report: LinkedReport[ElbKey, ElbReport], context: Option[String]): UIO[Unit] =
